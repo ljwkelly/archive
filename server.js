17f9c280175b83976add799304d5a41d62ed8a89
ljwkelly <151448926+ljwkelly@users.noreply.github.com>
2023-11-20 13:19:29 +0900

    server.js

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  // Listen for createWhiteCircle events from clients
+  socket.on('createWhiteCircle', (data) => {
+    // Broadcast the new white circle data to all connected clients
+    io.emit('updateCircles', { whiteCircles: data });
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
